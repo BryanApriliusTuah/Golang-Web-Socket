d92e541a55842d118cc6dbdd6624ff26d0218357
Bryan Aprilius Tuah <tuahayen@gmail.com>
2025-06-14 20:56:43 +0700

    Fetching Level API

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -15,6 +16,11 @@ var upgrader = websocket.Upgrader{
 	},
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+}
+
+type Level struct {
+	Normal int
+	Banjir int
 }
 
 var clients = make(map[*websocket.Conn]bool)
@@ -34,6 +40,30 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Client connected from : ", conn.RemoteAddr().String())
 
+	url := "http://localhost:3000/api/level"
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Error Fetching Level: ", err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println("Error Reading Body Request: ", err)
+		return
+	}
+
+	var level Level
+	err = json.Unmarshal(body, &level)
+	if err != nil {
+		fmt.Println("Failed Decode Body: ", err)
+	}
+
 	for {
 		messageType, msg, err := conn.ReadMessage()
 		if err != nil {
@@ -50,9 +80,9 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 
 		var status_elevation string
 		elevation := int(incoming["elevation"].(float64))
-		if elevation > 100 {
+		if elevation > level.Normal {
 			status_elevation = "Normal"
-		} else if elevation < 80 {
+		} else if elevation < level.Banjir {
 			status_elevation = "Banjir"
 		} else {
 			status_elevation = "Siaga"
